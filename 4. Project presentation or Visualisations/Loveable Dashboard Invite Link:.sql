a8ef1ae314941a94640ff9f3955eff3d8afc35de
tshepangmosalaesi@gmail.com <tshepangmosalaesi@gmail.com>
2026-08-01 14:21:56 +0000

    Bright Coffee Loveable Dashboard Invite Link

---
+++ 4. Project presentation or Visualisations/Loveable Dashboard Invite Link:.sql
@@ -0,0 +1,3 @@
+-- Databricks notebook source
+--Bright Coffee Loveable Dashboard Invite Link:
+https://lovable.dev/projects/58c4184b-1e05-4976-95d5-c47465851f46?magic_link=mc_7587fbd5-835c-4e1d-bdc8-3998bf2015c9
